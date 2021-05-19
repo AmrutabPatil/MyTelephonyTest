6cf313643b51eae115a3dad2930f5ac4a4d272a4
Amruta Patil <patilamruta4444@gmail.com>
2021-05-19 11:30:02 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/mytelephony/DeliveredActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mytelephony
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveredActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivered)
+    }
+}
